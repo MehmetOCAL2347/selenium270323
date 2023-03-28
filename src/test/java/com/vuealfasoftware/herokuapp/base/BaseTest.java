1e2aafdd97110c3ac9ddc87b9f2ea242c071d53a
Mehmet ÖCAL <ocalmehmet.2347@gmail.com>
2023-03-29 00:10:31 +0300

    BrowserFactory created and BaseTest edited

--- src/test/java/com/vuealfasoftware/herokuapp/base/BaseTest.java
+++ src/test/java/com/vuealfasoftware/herokuapp/base/BaseTest.java
@@ -8,12 +8,14 @@ import org.testng.annotations.BeforeMethod;
 public class BaseTest {
 
     protected WebDriver driver;
+    private BrowserFactory browserFactory;
 
     @BeforeMethod
     public void startUp(){
-        System.setProperty("webdriver.http.factory", "jdk-http-client");
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        driver= new ChromeDriver();
+        //System.setProperty("webdriver.http.factory", "jdk-http-client");
+        //System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        browserFactory = new BrowserFactory("chrome");
+        driver= this.browserFactory.createDriver();
         driver.manage().window().maximize();
     }
 
---
+++ src/test/java/com/vuealfasoftware/herokuapp/base/BrowserFactory.java
@@ -0,0 +1,29 @@
+package com.vuealfasoftware.herokuapp.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserFactory {
+
+    private WebDriver driver;
+    private String browser;
+
+    public BrowserFactory(String browser) {
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+        this.browser = browser.toLowerCase();
+    }
+
+    /* Refactor edilmeli interface ile çözülebilmeli burası*/
+    public WebDriver createDriver(){
+        if (browser.equals("firefox")) {
+            System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
+            this.driver = new FirefoxDriver();
+        }else {
+            System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+            this.driver = new ChromeDriver();
+        }
+        return this.driver;
+    }
+
+}
