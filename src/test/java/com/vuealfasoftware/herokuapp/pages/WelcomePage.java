c1a6fc6c92d077f9744141d96f72c49850cf0ce1
Mehmet ÖCAL <ocalmehmet.2347@gmail.com>
2023-03-29 22:49:41 +0300

    Lombok added

---
+++ src/test/java/com/vuealfasoftware/herokuapp/pages/WelcomePage.java
@@ -0,0 +1,13 @@
+package com.vuealfasoftware.herokuapp.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import java.util.logging.Logger;
+
+public class WelcomePage {
+
+    private WebDriver driver;
+    //private Logger log;
+    private String pageUrl = "https://vue-alfabank-demo.web.app/main";
+
+}
