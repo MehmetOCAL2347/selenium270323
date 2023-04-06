7e6fb53e563d0363a622620414e10c894c688c74
Mehmet ÖCAL <ocalmehmet.2347@gmail.com>
2023-04-07 00:26:37 +0300

    CSV file created and read for test datas

--- src/test/java/com/vuealfasoftware/herokuapp/alerttest/AlertTest.java
+++ src/test/java/com/vuealfasoftware/herokuapp/alerttest/AlertTest.java
@@ -1,18 +1,26 @@
 package com.vuealfasoftware.herokuapp.alerttest;
 
+import com.vuealfasoftware.herokuapp.base.CsvDataProviders;
 import com.vuealfasoftware.herokuapp.base.TestUtilities;
 import com.vuealfasoftware.herokuapp.pages.AlertsPage;
 import com.vuealfasoftware.herokuapp.pages.WelcomePage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Map;
+
 public class AlertTest extends TestUtilities {
 
     private String expectedAlertText = "I am a JS Alert";
     private String expectedResultText = "You successfully clicked an alert";
 
-    @Test
-    public void alertTest(){
+    @Test(dataProvider = "csvReader", dataProviderClass = CsvDataProviders.class)
+    public void alertTest(Map<String, String> testData){
+
+        String no = testData.get("no");
+        String alertMessage = testData.get("alertMessage");
+
+        System.out.println(no + " " + alertMessage);
 
         WelcomePage welcomePage = new WelcomePage(driver);
         welcomePage.openPage();
